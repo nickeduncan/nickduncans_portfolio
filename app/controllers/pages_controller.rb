643b821a73b54d837ca7d147413b63b9b42e966b
Nick Duncan <nickeduncan@gmail.com>
2017-03-25 17:23:57 -0400

    formatted navbar for rails

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def gallery
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'pages#index'
+  get '/gallery' => 'pages#gallery'
+  get '/about' => 'pages#about'
+  get '/contact' => 'pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
