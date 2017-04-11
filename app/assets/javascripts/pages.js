c7d9a677982c90613aa86ea5c8c1c1dba5f7795d
Nick Duncan <nickeduncan@gmail.com>
2017-04-11 19:23:13 -0400

    stripped out fullpage.js and changed pages to partials

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,5 +1,7 @@
 $(document).on('turbolinks:load', function() {
+
 	$('.menu_toggle').on('click', function() {
     $('.menu').slideToggle('400');
   });
+
 });
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#index'
-  get '/gallery' => 'pages#gallery'
-  get '/about' => 'pages#about'
-  get '/contact' => 'pages#contact'
+  get '/gallery' => 'pages#_gallery'
+  get '/about' => 'pages#_about'
+  get '/contact' => 'pages#_contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
