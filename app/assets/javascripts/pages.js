066262594d12f07295af2c5fbf81eea9e5b4dd98
Nick Duncan <nickeduncan@gmail.com>
2017-04-12 12:42:38 -0400

    single page works through arrows

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -5,7 +5,33 @@ $(document).on('turbolinks:load', function() {
   });
 
 	$('.glyphicon-menu-right').on('click', function() {
-		$('.index').toggle('slide');
-		$('.about').toggle('slide');
+		if ($('.index').hasClass('active')) {
+			$('.index').removeClass('active');
+			$('.about').addClass('active');
+		} else if ($('.gallery').hasClass('active')) {
+			$('.gallery').removeClass('active');
+			$('.index').addClass('active');
+		}
 	});
+
+	$('.glyphicon-menu-left').on('click', function() {
+		if ($('.index').hasClass('active')) {
+			$('.index').removeClass('active');
+			$('.gallery').addClass('active');
+		} else if ($('.about').hasClass('active')) {
+			$('.about').removeClass('active');
+			$('.index').addClass('active');
+		}
+	});
+
+	$('.glyphicon-menu-down').on('click', function() {
+		$('.index').removeClass('active');
+		$('.contact').addClass('active');
+	});
+
+	$('.glyphicon-menu-up').on('click', function() {
+		$('.contact').removeClass('active');
+		$('.index').addClass('active');
+	});
+
 });
